3c8b6186467b4f4325446ca40222963511910576
Guzu Pavel <pgutzu@gmail.com>
2018-04-03 19:24:23 +0300

    database

--- controllers/service.js
+++ controllers/service.js
@@ -11,7 +11,7 @@ module.exports = (serviceService, config) => {
     router.get('/get', (req, res) => {
         serviceService.getServices()
             .then((data) => {
-
+                res.header("Access-Control-Allow-Origin: *")
                 res.send(data);
             })
             .catch((err) => res.error(err));
