a07be21f13e2a94d1279e3905f37cf36d0facfd7
Guzu Pavel <pgutzu@gmail.com>
2018-04-03 19:04:45 +0300

    database

--- server.js
+++ server.js
@@ -1,29 +1,12 @@
-/**
- * Created by Pavel on 01.07.2017.
- */
-const express =  require("express");
-const bodyParser = require('body-parser');
-const Sequelize = require('sequelize');
-const Mustache = require('mustache');
-const config = require('./tsconfig');
-const dbcontext = require('./context/db')(Sequelize,config);
-
-const serviceService = require('./service/service')(dbcontext.service);
-const orderService = require('./service/order')(dbcontext.basket);
-const apiController = require('./controllers/api')(serviceService,orderService,config);
-const logger = require('./utils/logger');
+const express = require('express');
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use('/api',logger);
-app.use('/api',apiController);
-dbcontext.sequelize
-    .sync()
-    .then(() =>
-    {
-        app.listen(3000, () => console.log('---server is running---'));
-    })
-    .catch((err) => console.log(err));
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello New World!');
+});
+
+app.listen(port, function () {
+    console.log(`Server running on ${port}`);
+});
